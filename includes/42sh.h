ab69745a9d39ddd90ddf9a2fb76bd8a387652e11
le-dio_l <leo.le-diouron@Epitech.Eu>
2016-04-26 10:55:07 +0200

    .h --> 1

--- includes/42sh.h
+++ includes/42sh.h
@@ -5,7 +5,7 @@
 ** Login   <chabot_t@epitech.net>
 ** 
 ** Started on  Tue Apr 26 09:15:14 2016 Thomas CHABOT
-** Last update Tue Apr 26 09:38:41 2016 Thomas CHABOT
+** Last update Tue Apr 26 10:53:29 2016 leo LE DIOURON
 */
 
 #ifndef SH42_H_
@@ -14,6 +14,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <curses.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
@@ -22,14 +23,29 @@
 
 /* **************** MYLIB ****************** */
 
-void		my_putchar(char);
+void		my_putchar(char, int);
 
-void		my_putstr(char *);
+void		my_putstr(char *, int);
+
+void		my_put_nbr(int, int);
+
+int		my_getnbr(char *);
 
 int		my_strlen(char *);
 
 void		my_show_tab(char **);
 
 int		count_tab(char **);
+
+char            *get_next_line();
+char		*mallc_lien(char *);
+
+char		*my_epur_str(char *);
+
+int		my_strcmp(char *, char*);
+
+void		my_free(void *);
+
+void		*my_mallok(void *, int);
 
 #endif /* SH42_H_ */
