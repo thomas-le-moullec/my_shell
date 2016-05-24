7eac8fd282b9c19288149c6b005eac234e28c0ee
chabot_t <thomas.chabot@epitech.eu>
2016-05-24 16:07:36 +0200

    ok

---
+++ src/exec/signal.c
@@ -0,0 +1,32 @@
+/*
+** signal.c for signal in /home/chabot_t/rendu/PSU/PSU_2015_42sh
+** 
+** Made by Thomas CHABOT
+** Login   <chabot_t@epitech.net>
+** 
+** Started on  Tue May 24 13:51:52 2016 Thomas CHABOT
+** Last update Tue May 24 14:03:39 2016 Thomas CHABOT
+*/
+
+#include "42sh.h"
+
+int			check_signal(int status)
+{
+  int			i;
+  static t_signal	signal[] = {
+    {SIGABRT, SIG_SIGABRT},
+    {SIGFPE, SIG_SIGFPE},
+    {SIGQUIT, SIG_SIGQUIT},
+    {SIGSEGV, SIG_SIGSEGV},
+    {SIGILL, SIG_SIGILL},
+  };
+
+  i = 0;
+  while (i < NB_SIGNALS)
+    {
+      if (status == signal[i].status)
+	my_putstr(signal[i].msg, 1);
+      i++;
+    }
+  return (SUCCESS);
+}
